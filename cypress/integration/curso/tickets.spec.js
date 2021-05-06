714962ab09cc5420df7b78744fa954fb876f3de7
ericksonmartinez <erickson.martinez.before.com>
2021-05-05 21:15:12 -0400

    Preenche inputs

--- cypress/integration/curso/tickets.spec.js
+++ cypress/integration/curso/tickets.spec.js
@@ -1,6 +1,18 @@
-describe('tickets', () => {
-    beforeEach(()=>{cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html")})
-  it('confimando pagina de ticket box', () => {
+/// <reference types="cypress"/>
 
+describe('tickets', () => {
+  beforeEach(()=>{cy.visit("http://bit.ly/2XSuwCW")})
+  it('confimando pagina de ticket box', () => {
+    cy.get('h1').should('contain', 'TICKETBOX')
   });
+
+  it.only("preencher todos os campos inputs", ()=> {
+    const firsName = "rafael"
+    cy.get('#first-name').type(`${firsName}`);
+    cy.get('#last-name').type('souza');
+    cy.get('#email').type('test@test.com');
+    cy.get('#requests').type('muito bom curso de cypress');
+    cy.get('#signature').type(firsName);
+
+  })
 });
