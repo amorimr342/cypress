4f527e118278e7259e528e44746e704c799eef7d
ericksonmartinez <erickson.martinez.before.com>
2021-05-05 20:09:55 -0400

    meu primeiro test

---
+++ cypress/integration/curso/tickets.spec.js
@@ -0,0 +1,6 @@
+describe('tickets', () => {
+    beforeEach(()=>{cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html")})
+  it('confimando pagina de ticket box', () => {
+
+  });
+});
