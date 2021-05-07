79c6139e318012681f68655d87738974fc4de9de
ericksonmartinez <erickson.martinez.before.com>
2021-05-06 21:47:47 -0400

    preencher e resete do formulario

--- cypress/integration/curso/tickets.spec.js
+++ cypress/integration/curso/tickets.spec.js
@@ -50,5 +50,36 @@ describe('tickets', () => {
     cy.get("@email").clear().type("test@test.com");
     cy.get("#email.invalid").should("not.exist");
 
+
+  });
+  it.only('prenecher e resetar formulario', () => {
+    const firstName = "rafael";
+    const lastName = "souza";
+    const fullName =`${firstName} ${lastName}`;
+    cy.get('#first-name').type(`${firstName}`);
+    cy.get('#last-name').type(lastName);
+
+    cy.get('#email').type('test@test.com');
+    cy.get('#requests').type('muito bom curso de cypress');
+     
+    cy.get('#ticket-quantity').select("3");
+
+    cy.get('#social-media').check();
+    cy.get('#publication').check();
+    cy.get('#vip').check();
+
+    cy.get(".agreement p").should(
+      "contain",
+      `I, ${fullName}, wish to buy 3 VIP tickets.`);
+      cy.get("#agree").click();                 
+      cy.get('#signature').type(fullName);
+
+      cy.get("button[type='submit']")
+        .as("submitButton")
+        .should("not.be.disabled");
+
+        cy.get("button[type='reset']").click();
+
+        cy.get("@submitButton").should("be.disabled");
   });
 });
